669a55aee93d85fb169f09328d1da660aee8d2ad
Georgiy Deliya <gosha@georgiys-mini.myfiosgateway.com>
2024-01-26 16:01:34 -0500

    adding the initial commit

--- src/test/java/com/jcg/cucumber/CalculatorTest.java
+++ src/test/java/com/jcg/cucumber/CalculatorTest.java
@@ -3,8 +3,11 @@ package com.jcg.cucumber;
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
-
+//testing
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/resources")
 public class CalculatorTest {
 }
+
+
+//testing
